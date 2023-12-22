5656f0b7c1ae66d92d1050827f3f2a3f37af71f7
MohamedBoukthir <medboukthir7@gmail.com>
2023-12-22 20:40:08 +0100

    Project Initialization

---
+++ src/main/java/com/emp/empmanager/EmpManagerApplication.java
@@ -0,0 +1,13 @@
+package com.emp.empmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpManagerApplication.class, args);
+    }
+
+}
